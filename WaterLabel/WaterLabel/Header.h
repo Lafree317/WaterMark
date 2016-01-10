469005e808113b86a8e79d09d4a4baf4b89d176d
Lafree317 <lafree317@163.com>
2016-01-10 20:10:13 +0800

    解决OCBlock

---
+++ WaterLabel/WaterLabel/Header.h
@@ -0,0 +1,19 @@
+//
+//  Header.h
+//  WaterLabel
+//
+//  Created by huchunyuan on 16/1/5.
+//  Copyright © 2016年 huchunyuan. All rights reserved.
+//
+
+#ifndef Header_h
+#define Header_h
+
+
+#import "MLSelectPhoto.h"
+#import "MLSelectPhotoPickerViewController.h"
+
+//#import "MLSelectPhotoPickerViewController"
+
+
+#endif /* Header_h */
--- WaterLabel/WaterLabel/ViewController.swift
+++ WaterLabel/WaterLabel/ViewController.swift
@@ -9,14 +9,32 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+    
+    }
+    // 修改状态栏颜色
+    override func preferredStatusBarStyle() -> UIStatusBarStyle {
+        // 设置状态栏颜色为白色
+        return UIStatusBarStyle.LightContent
     }
     // 添加水印
     @IBAction func addWaterAction(sender: UIButton) {
+        let addWaterVC = MLSelectPhotoPickerViewController()
+        self.showViewController(addWaterVC, sender: nil)
+        // Swfit调用OC的Block的方法{(名称) -> in   }
+        addWaterVC.callBack = { (AnyObject) -> () in
+            print(AnyObject);
+            self.dismissViewControllerAnimated(true, completion: { () -> Void in
+                
+            });
+        }
+
         
+        
+//        AddWaterVC.delegate = self;
+    
     }
     // 默认水印
     @IBAction func defaultAction(sender: UIButton) {
